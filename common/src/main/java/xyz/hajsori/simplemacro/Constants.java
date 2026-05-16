828b3a6052ade7122c0167b6803b5b6f22c7dd3d
Hajsori <hajsori@proton.me>
2026-05-16 17:23:54 +0200

    Changed Version to 1.2.1

--- common/src/main/java/xyz/hajsori/simplemacro/Constants.java
+++ common/src/main/java/xyz/hajsori/simplemacro/Constants.java
@@ -7,7 +7,7 @@ import xyz.hajsori.simplemacro.config.ClientConfig;
 public class Constants {
     public static final String MOD_ID = "simplemacro";
     public static final String MOD_NAME = "Simple Macro";
-    public static final String MOD_VERSION = "1.1.1+26.1";
+    public static final String MOD_VERSION = "1.2.0+26.1";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
     public static ClientConfig CLIENT_CONFIG;
 }
